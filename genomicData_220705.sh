5c68417a5cd85d94c7c532eb5942b25ed7596c7d
bhavaniuppaluri <51313638+bhavaniuppaluri@users.noreply.github.com>
2022-11-02 17:08:15 -0500

    Update genomicData_220705.sh

--- genomicData_220705.sh
+++ genomicData_220705.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+#SBATCH --partition=normal
+#SBATCH --ntasks=1
+#SBATCH --mem=1024
+#SBATCH --output=mousedata_%J_stdout.txt
+#SBATCH --error=mousedata_%J_stderr.txt
+#SBATCH --time=12:00:00
+#SBATCH --job-name=mousedata
+#SBATCH --mail-user=lakshmibhavaniuppaluri-1@ou.edu
+#SBATCH --mail-type=ALL
+#SBATCH --chdir=/home/bhavani/RNAseq-analysis/
+#
 
 # BASH script to retrieve genomic data for project 220705
 # usage: bash genomicData_220705.sh outputsPath
@@ -15,3 +27,5 @@ wget https://ftp.ensembl.org/pub/release-108/fasta/mus_musculus/dna/Mus_musculus
 
 # retrieve the gene annotations
 wget https://ftp.ensembl.org/pub/release-108/gtf/mus_musculus/Mus_musculus.GRCm39.108.gtf.gz
+
+
